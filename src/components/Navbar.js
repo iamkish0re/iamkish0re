0d8c40335030b7800632395fde76809d47aa14da
iamkish0re <kishoresreedhar1999@gmail.com>
2023-02-04 20:31:39 +0530

    Added Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
     <div className='fixed z-50 bg-dark-500 w-full top-0 left-0 px-8 py-4 lg:px-20 xl:px-36'>
         <div className="flex justify-between items-center text-white">
             {/* <img src={logo} className="App-logo w-10 rounded-full" alt="logo" /> */}
-            <ul className="hidden md:flex">
+            <ul className="hidden inline">
             <li className="p-4"><a href="#home" className="hover:underline">About</a></li>
             <li className="p-4"><a href="#skills" className="hover:underline">Skills</a></li>
             <li className="p-4"><a href="#honors" className="hover:underline">Honor & Awards</a></li>
